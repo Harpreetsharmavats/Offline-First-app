9cb847a486b243347799fc564019ef492661901e
Harpreetsharmavats <harpreetsharma902@gmail.com>
2026-06-10 17:32:26 +0530

    changes

--- app/src/main/java/com/example/offlinefirstapp/data/remote/WallpaperDto.kt
+++ app/src/main/java/com/example/offlinefirstapp/data/remote/WallpaperDto.kt
@@ -5,22 +5,9 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class WallpaperDto(
     val id: String,
-    val urls: WallpaperUrlsDto,
-    val user: UserDto,
-    val alt_description: String? = null
-)
-
-@Serializable
-data class WallpaperUrlsDto(
-    val raw: String,
-    val full: String,
-    val regular: String,
-    val small: String,
-    val thumb: String
-)
-
-@Serializable
-data class UserDto(
-    val name: String,
-    val username: String
+    val author: String,
+    val width: Int,
+    val height: Int,
+    val url: String,
+    val download_url: String
 )
